71906ebcceef00b3221f5acf0cbb07e7c86d6c31
Khitish <kitzx1999@gmail.com>
2025-04-22 00:14:39 +0530

    Added tailwind.config

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./app/**/*.{js,ts,jsx,tsx}",  // for App Router
+      "./pages/**/*.{js,ts,jsx,tsx}", // for Pages Router
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
